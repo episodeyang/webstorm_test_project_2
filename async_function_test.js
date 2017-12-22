f1a86e8576b64cf0482539e050e440ffdf5afae8
Ge Yang <yangge1987@gmail.com>
2017-12-22 12:58:19 -0600

    Add comments

--- async_function_test.js
+++ async_function_test.js
@@ -1,7 +1,9 @@
+/* This does not work, without an export keyword */
 async function test(){
     return await new Promise((res, rej) => setTimeout(() => res(10), 1000));
 }
 
+/* Now, this works with export. Why? */
 // export async function test() {
 //     return await new Promise((res, rej) => setTimeout(() => res(10), 1000));
 // }
