5caebf2c5be097ab2ec2c34fc8353f13073cdb10
Ge Yang <yangge1987@gmail.com>
2017-12-22 12:56:49 -0600

    Example showing async inside generator doesn't work without export

--- async_function_test.js
+++ async_function_test.js
@@ -6,4 +6,10 @@ async function test(){
 //     return await new Promise((res, rej) => setTimeout(() => res(10), 1000));
 // }
 
+function* Search(){
+    let a = yield test();
+    console.log(a)
+}
+
 test().then(a => console.log(a));
+Search().next(20);
